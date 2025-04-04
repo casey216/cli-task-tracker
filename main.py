17c83dd0261c1a34e91ec572d3539644551d1399
Kenechi <caseynzewi@gmail.com>
2025-04-04 16:26:45 +0100

    create switch statements to handle commands

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 import os
+import sys
 
-def main(*args, **kwargs):
+def main():
 
     # create database file
     filename = "tasks.json"
     if not os.path.exists(filename):
         with open(filename, "w") as db:
             pass
+
+    if len(sys.argv) < 2:
+        print("Usage: python main.py <command> [arg]")
+        return
+    
+    match sys.argv[1]:
+        case 'add':
+            pass
+        case 'update':
+            pass
+        case 'delete':
+            pass
+        case 'mark-in-progress':
+            pass
+        case 'mark-done':
+            pass
+        case 'list':
+            pass
+        case _:
+            pass
+
+
+if __name__ == "__main__":
+    main()
